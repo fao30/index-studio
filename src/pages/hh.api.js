4fff733835efa17bd5ee117b94ee20bb21fe1d63
fao30 <fakhrularifin@windowslive.com>
2023-04-15 00:31:38 +0300

    URL added

--- src/pages/hh.api.js
+++ src/pages/hh.api.js
@@ -2,12 +2,16 @@ import api from "../api";
 
 export default class ApiCall {
   static async getProfile(page) {
-    let url = `/frontend-test/api/v1/items?page=${page}`;
+    let url = `https://testguru.ru/frontend-test/api/v1/items?page=${page}`;
+    //ERROR DURING DELPOY
+    // let url = `/frontend-test/api/v1/items?page=${page}`;
 
     return await api.get(url);
   }
   static async getDetail(id) {
-    let url = `/frontend-test/api/v1/items/${id}`;
+    let url = `https://testguru.ru/frontend-test/api/v1/items/${id}`;
+    //ERROR DURING DELPOY
+    // let url = `/frontend-test/api/v1/items/${id}`;
 
     return await api.get(url);
   }
